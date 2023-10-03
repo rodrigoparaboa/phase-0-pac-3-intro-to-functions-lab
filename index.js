e5bd9a788bf3cd7a27e25144467222c111bd476a
rodrigoparaboamartins <rodrigoparaboa@hotmail.com>
2023-10-03 18:44:23 +1000

    Completed Assignemnt

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function shout(string){
+function shout(string) {
     return string.toUpperCase();
 }
 
@@ -7,15 +7,19 @@ function whisper(string) {
 }
 
 function logShout(string) {
-    console.log(logShout("HELLO"))
-  }
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
 
 function sayHiToHeadphonedRoommate(string) {
-    if (string === string.toLowerCase()){
+    if (string === string.toLowerCase()) {
         return "I can't hear you!";
-    }   else if (string === string.toUpperCase()) {
+    } else if (string === string.toUpperCase()) {
         return "YES INDEED!";
-    }   else if (string === "Let's have dinner together!") {
+    } else if (string === "Let's have dinner together!") {
         return "I would love to!";
     }
 }
